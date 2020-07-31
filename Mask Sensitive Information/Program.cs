016cc32637357f903f295c446b8871b29eeef168
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-07-30 20:53:24 -0500

    Add project files.

---
+++ Mask Sensitive Information/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Mask_Sensitive_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter in a secret.");
+            var secret = Console.ReadLine();
+            string final = "";
+
+            secret.ToCharArray();
+            for (int i = 0; i < secret.Length; i++)
+            {
+                if (secret.Length - 4 > i)
+                {
+                    final += "*";
+                } else
+                {
+                    final += secret[i];
+                }
+            }
+            Console.WriteLine(final);
+            Console.Read();
+        }
+    }
+}
